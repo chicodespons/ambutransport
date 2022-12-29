5ed51d5f8a81822894c3918ca7f9fab3cc6fa12a
chicodespons <pieterdedycker@gmail.com>
2022-12-29 10:11:50 +0100

    first commit

---
+++ src/main/java/com/chicodespons/ambutransport/AmbutransportApplication.java
@@ -0,0 +1,13 @@
+package com.chicodespons.ambutransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbutransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbutransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chicodespons/ambutransport/AmbutransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chicodespons.ambutransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbutransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
